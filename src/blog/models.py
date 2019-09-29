0dccc67dd1a26db9f51481053d5a6a1408e4a26b
Asaad <asad2010222@gmail.com>
2019-09-29 13:59:34 +0200

    initial models and admin on databases

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    post_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ('-post_date', )
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,38 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'title': 'التدوينة الاولي',
-        'content': 'نص التدوينة الاولي كنص تجريبى ',
-        'post_date':'29/9/2019',
-        'auther':'Asaad Mohammed',
-    },
-    {
-        'title': 'التدوينة الثانية',
-        'content': 'نص التدوينة الثانية كنص تجريبى ',
-        'post_date':'30/3/2019',
-        'auther':'Asaad Mohammed',
-    },
-    {
-        'title': 'التدوينة الثالثة',
-        'content': 'نص التدوينة الثالثة كنص تجريبى ',
-        'post_date':'16/9/2019',
-        'auther':'Asaad Mohammed',
-    },
-    {
-        'title': 'التدوينة الرابعة',
-        'content': 'نص التدوينة الرابعة كنص تجريبى ',
-        'post_date':'29/1/2019',
-        'auther':'Asaad Mohammed',
-    },
-]
+
 
 def home(request):
     context = {
         'title':'الصفحة الرئيسية',
-        'posts':posts
+        'posts':Post.objects.all(),
 
     }
     return render(request, 'blog/index.html', context)
