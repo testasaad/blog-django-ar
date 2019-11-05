c77d29a54c8ad5527c47028f68f1843730ed63aa
testasaad <testasaad@github.com>
2019-11-05 11:25:00 +0200

    edit project and del som files

---
+++ venv_win/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\workcoding\webproject\projectdjango\tutorials\blogger\venv_win\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
