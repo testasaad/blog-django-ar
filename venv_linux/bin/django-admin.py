7a835846e29f3c16605cf5016b1d3ca8c92cbddb
testasaad <testasaad@github.com>
2019-11-05 11:53:31 +0200

    Removed all files that are in the .gitignore delete venv

--- venv_linux/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/bb/7AEAF7D3EAF78A21/workCoding/webProject/projectDjango/tutorials/Blogger/venv_linux/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv_win/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\workcoding\webproject\projectdjango\tutorials\blogger\venv_win\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
